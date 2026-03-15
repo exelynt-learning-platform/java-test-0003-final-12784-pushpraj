7463c5581f495577ba649aefc0aa866a13f98ab3
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-15 21:35:54 +0530

    Update BinaryPyramid.java

--- BinaryPyramid.java
+++ BinaryPyramid.java
@@ -3,7 +3,7 @@ public class BinaryPyramid {
     final int TOTAL_ROWS = 6;
     for(int row = 1; row <= TOTAL_ROWS; row++){
         for(int column = 0;  column < row; column++){
-            System.out.print((row+column)%2);
+            System.out.print((row+column)%2 + " ");
         }
         System.out.println();
     }
