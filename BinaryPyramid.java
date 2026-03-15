0452943fb99a667146ce147dd294ccaf28d9fe95
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-15 21:38:06 +0530

    Update BinaryPyramid.java

--- BinaryPyramid.java
+++ BinaryPyramid.java
@@ -1,8 +1,14 @@
 public class BinaryPyramid {
   public static void main(String[] args){
-    final int TOTAL_ROWS = 6;
+    
+    // Total number of rows in our pyramid
+    final int TOTAL_ROWS = 6; 
+
+    // Outer loop for 6 rows
     for(int row = 1; row <= TOTAL_ROWS; row++){
+      // Inner loop for dynamic column numbers
         for(int column = 0;  column < row; column++){
+            // Printing logic, remainder of row + column
             System.out.print((row+column)%2 + " ");
         }
         System.out.println();
